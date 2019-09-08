d72fcc6d6cb7f230ce8d8c2db6945107baf9cc72
sdMac <553828127@qq.com>
2019-09-08 10:20:20 +0800

    add toast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  msgs = [];
   title = 'ngBlog';
+
+  ngOnInit() {
+    this.msgs = [{ severity: 'success', summary: '摘要', detail: '测试换行，测试换行，测试换行' }];
+  }
 }
